a374d4c532d2697f670ae19202704584ce3c2e19
João Marcos da Costa <costa.joaomarcos@gmail.com>
2018-11-10 17:24:10 -0200

    Criação do checkout

--- app/src/main/java/br/com/joaomarcos/helloandroid/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/joaomarcos/helloandroid/ui/main/MainActivity.kt
@@ -1,18 +1,47 @@
 package br.com.joaomarcos.helloandroid.ui.main
 
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.joaomarcos.helloandroid.R
+import br.com.joaomarcos.helloandroid.model.Pedido
+import br.com.joaomarcos.helloandroid.ui.checkout.CheckoutActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nome = intent.getStringExtra("nome")
-        val telefone = intent.getStringExtra("telefone")
 
-        tvNome.text = getString(R.string.saudacao, nome, telefone)
+        mainViewModel = ViewModelProviders
+                .of(this)
+                .get(MainViewModel::class.java)
+
+        mainViewModel.nomeCliente = intent.getStringExtra("nome")
+        mainViewModel.telefoneCliente = intent.getStringExtra("telefone")
+
+        tvNome.text = getString(R.string.saudacao, mainViewModel.nomeCliente, mainViewModel.telefoneCliente)
+
+        cbAtum.isChecked = mainViewModel.atumSelecionado
+        cbBacon.isChecked = mainViewModel.baconSelecionado
+        cbCalabresa.isChecked = mainViewModel.calabresaSelecionada
+        cbMussarela.isChecked = mainViewModel.mussarelaSelecionada
+
+        btnCalcular.setOnClickListener {
+            val intent = Intent(this,
+                    CheckoutActivity::class.java)
+            intent.putExtra("pedido", gerarPedido())
+            startActivity(intent)
+        }
+
+    }
+
+    private fun gerarPedido(): Pedido {
+        return Pedido(mainViewModel.nomeCliente,
+                mainViewModel.telefoneCliente)
     }
 }
--- app/src/main/java/br/com/joaomarcos/helloandroid/ui/main/MainViewModel.kt
+++ app/src/main/java/br/com/joaomarcos/helloandroid/ui/main/MainViewModel.kt
@@ -1,2 +1,14 @@
 package br.com.joaomarcos.helloandroid.ui.main
 
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var nomeCliente = ""
+    var telefoneCliente = ""
+    var atumSelecionado =  false
+    var baconSelecionado = false
+    var calabresaSelecionada = false
+    var mussarelaSelecionada = false
+
+}
